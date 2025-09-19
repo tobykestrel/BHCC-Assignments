2feb81c5536539f4572fe23ba3514feb2f97dc53
tobykestrel <tobykestrel@gmail.com>
2025-09-18 23:22:32 -0400

    Finished Data Structures StackQueueDeleteNodeHW

---
+++ 3. Data Structures Fall 2025/04. QueueHW2/QUEUE.C
@@ -0,0 +1,255 @@
+
+
+//queue
+
+#include <stdio.h>
+#include <alloc.h> //malloc.h   stdlib.h
+#include <ctype.h>
+#include <conio.h>
+
+struct entry
+        {
+	char name[81];
+        } part;
+
+typedef struct LISTREC
+        {
+        struct entry info;
+	struct LISTREC * link;
+	}  LISTREC;
+
+
+LISTREC *next;
+
+LISTREC *top , *purgequeue(), *ne, *p, *last;
+void push(), pop();
+void printqueue();
+void loadfile();
+void countqueue();
+void menu();
+void listdelete();
+
+
+FILE *stream1,*stream2;
+void savequeue();
+
+
+void main()
+
+{
+        char selection;
+        top = NULL;
+
+
+        stream1 = fopen("data.bin","r+b");
+
+
+        /* initialize */
+    loadfile();
+
+        clrscr();
+
+        for (;;) {
+        menu ();
+        selection = getche();
+	switch (toupper(selection))
+	{
+	    case '1' : push();clrscr() ; break;
+	    case '2' : pop(); clrscr() ; break;
+	    case '3' : printqueue(top) ; clrscr() ; break;
+	    case '4' : loadfile() ; clrscr() ; break;
+	    case '5' : savequeue() ; clrscr(); break;
+	    case '6' : countqueue() ; clrscr(); break;
+	    case '7' : top = purgequeue(top) ; clrscr(); break;
+		case '8' : listdelete() ; clrscr(); break;
+	    case 'Q' : clrscr() ; return ;//break;
+	      }  /*  switch  */
+
+              }  /* for  */
+
+} /* main */
+
+
+void push()
+{
+clrscr();
+gotoxy(10,12);
+printf("\n name to add ");
+scanf(" %[^\n]",part.name);
+ne=(LISTREC *) malloc(sizeof(LISTREC));
+ne->info=part;
+ne->link=NULL;
+
+
+
+
+if (! top) top=last=ne;
+	else { last->link=ne; last=ne; }
+
+
+
+//ne->link=top;
+//top=ne;
+
+
+}//push
+
+
+void printqueue(LISTREC *top)
+	{
+	clrscr() ;
+	printf ("\n\n");
+
+	while (top){
+
+	 printf("\n %s ", top->info.name);
+                top = top->link;
+                } /* while */
+                gotoxy(52,25) ; printf("Press Any Key to Continue") ; getch();
+	} /* printqueue */
+
+
+void pop ()
+        {
+
+        clrscr();
+        gotoxy(10,12);
+	if (! top) { printf("\n\a the queue is empty !!!"); getch(); return;}
+        p=top;
+        top=top->link;
+	printf("\n %s",p->info.name);
+        getch();
+        free(p);
+        }  /* pop  */
+
+void loadfile ()
+        {
+        int n;
+        rewind(stream1);
+        for(;;) {
+        n=fread(&part,sizeof(part),1, stream1);
+        if (!n) break;
+        ne=(LISTREC *) malloc(sizeof(LISTREC));
+        ne->info=part; ne->link=NULL;
+
+
+        if (! top) top=last=ne;
+        else { last->link=ne; last=ne; }
+
+
+
+        }//for
+        }//loadfile
+
+void savequeue ()
+        {
+		       stream2 = fopen("output.bin","w+b");
+		       p = top;
+		       while (p)
+		       {
+
+		    fwrite(&p->info,sizeof(part),1,stream2);
+
+		// fprintf(stream2,"\n %s",p->info.name);
+                       p=p->link;
+                       }
+		      fclose(stream2);
+        }
+		
+
+void countqueue ()
+        {
+                       int i = 0;
+                       p = top;
+		       while (p)
+		       {
+		       i++;
+		       p=p->link;
+                       }
+                       clrscr() ; gotoxy(27,12) ;
+		       printf("Total Items in queue - %d",i);
+                       gotoxy(52,25) ; printf("Press Any Key to Continue");
+                       getch();
+        }
+
+
+
+
+void menu ()
+        {
+        gotoxy(30,1)  ; printf("NORTHEASTERN UNIVERSITY");
+        gotoxy(32,2)  ; printf ("University College");
+        gotoxy(37,3)  ; printf ("MIS 4277");
+	gotoxy(19,9)  ; printf (" 1 - Add an Item to the queue		 ");
+	gotoxy(19,10) ; printf (" 2 - Delete an Item from the queue	    ");
+	gotoxy(19,11) ; printf (" 3 - List the queue from Top to Bottom        ");
+	gotoxy(19,12) ; printf (" 4 - Load File to Current queue  ");
+	gotoxy(19,13) ; printf (" 5 - Save Current queue in File OUTPUT.BIN ");
+	gotoxy(19,14) ; printf (" 6 - Count Number of Items in queue	   ");
+	gotoxy(19,15) ; printf (" 7 - Purge the Entire queue		   ");
+	gotoxy(19,16) ; printf (" 8 - Delete from any location in the queue		   ");
+	
+        gotoxy(19,17) ; printf (" Q - End the program                          ");
+        gotoxy(25,22) ; printf ("Please enter your selection - ");
+        }
+
+
+LISTREC *purgequeue (LISTREC *top)
+
+        {
+                p = top;
+                while (p)
+                {
+                top = top->link;
+                if (p == last) free(last);
+				free(p);
+                p=top;
+                } /* while */
+		clrscr() ; gotoxy(27,12) ; printf("queue has been Purged");
+                gotoxy(52,25) ; printf("Press Any Key to Continue -") ; getch();
+                return (top);
+	} /* purgequeue */
+	
+
+
+
+void listdelete() {
+        int i=0; //FLAG FOR TRUE OR FALSE!!!!FOR THE TOP ITEM BEING DELETED!!!!
+        LISTREC *last,*next;
+        clrscr();
+        gotoxy(10,12);
+        printf("\n name to delete ");
+        scanf(" %[^\n]",part.name); //LISTREC *last,*next; // Can't do this in the language C
+        if (top == NULL) {
+                clrscr();
+                gotoxy(10,12);
+                printf("\n\a  The queue is EMPTY!!! ");
+                getch();
+                return;
+        }
+        next = top;
+        if ((strcmp(part.name,next->info.name) == 0) && (i==0)) { /*if both are same*/
+                top = top->link;
+                free(next);
+                return;
+        }
+        next = top;
+        while ((strcmp(part.name,next->info.name)!=0) && (next->link != NULL)) {
+                last = next;
+                next = next->link;        
+        }
+        if ((strcmp(part.name,next->info.name)==0)) {	
+                last->link=next->link;   
+                free(next);
+                return;
+        }
+        if ((strcmp(part.name,next->info.name) != 0)) {	
+                clrscr();
+                gotoxy(10,12);    
+                printf("\n\a %s not found \n",part.name);
+                getch();
+                return;	
+        }
+}	
+	
+	
---
+++ 3. Data Structures Fall 2025/05. StackQueueDeleteNodeHW/TobyHansen_StackQueueDeleteNodeHW.cpp
@@ -0,0 +1,227 @@
+/*======================================
+| Toby Hansen, September 18 2025.      |
+| StackQueueDeleteNodeHW, CSC-242.     |
+| This program is able to delete nodes |
+| from both stack and queue structures.|
+======================================*/
+
+#include <iostream>
+using namespace std;
+
+// Node structure containing an int and a pointer to another node.
+struct Node {
+	int data;
+	Node* next;
+};
+
+// Base LinkedList class
+class LinkedList {
+protected:
+    Node* head;   // points to first node
+    Node* tail;   // points to last node (helpful for queue)
+public:
+    LinkedList() : head(nullptr), tail(nullptr) {}
+    ~LinkedList() { clear(); }
+
+    void insertFront(int val) {
+        Node* newNode = new Node{val, nullptr};
+        if (!head) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    void insertBack(int val) {
+        Node* newNode = new Node{val, nullptr};
+        if (!tail) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    int removeFront() {
+        if (!head) { throw runtime_error("List is empty!"); } 
+        Node* temp = head;
+        int val = temp->data;
+        head = head->next;
+        if (!head) tail = nullptr;
+        delete temp;
+        return val;
+    }
+	
+	bool removeValue(int x) {
+        if (!head) return false;
+        if (head->data == x) {
+            Node* temp = head;
+            head = head->next;
+            if (!head) tail = nullptr;
+            delete temp;
+            return true;
+        }
+        Node* prev = head;
+        Node* curr = head->next;
+        while (curr) {
+            if (curr->data == x) {
+                prev->next = curr->next;
+                if (curr == tail) tail = prev;
+                delete curr;
+                return true;
+            }
+            prev = curr;
+            curr = curr->next;
+        }
+        return false;
+    }
+
+    void display() {
+        Node* curr = head;
+        while (curr) {
+            cout << curr->data << " -> ";
+            curr = curr->next;
+        }
+        cout << "NULL\n";
+    }
+
+    void clear() {
+        while (head) {
+            removeFront();
+        }
+    }
+};
+
+// Stack class (LIFO) derived from LinkedList
+class Stack : public LinkedList {
+public:
+    void push(int val) { insertFront(val); } // push to front/top
+    int pop() { return removeFront(); } // pop from front/top
+};
+
+// Queue class (FIFO) derived from LinkedList
+class Queue : public LinkedList {
+public:
+    void enqueue(int val) { insertBack(val); } // enqueue at back
+    int dequeue() { return removeFront(); } // dequeue from front
+};
+
+// This section is for declaring function prototypes.
+void enqueueToQueue(); 	// Enqueue a new node to the queue.
+void pushToStack(); 	// Push a new node to the stack.
+void removeFromList(); 	// Remove a node from a list.
+void displayLists(); 	// Display lists' contents.
+void printHelpText(); 	// Display the help text.
+
+// This section is for global variables.
+Queue q;
+Stack s;
+
+// This is the main function.
+int main() {
+
+    // This code banner is printed before the code is run.
+    cout<< "/*======================================\n"
+        << "| Toby Hansen, September 18 2025.      |\n"
+        << "| StackQueueDeleteNodeHW, CSC-242.     |\n"
+        << "| This program is able to delete nodes |\n"
+        << "| from both stack and queue structures.|\n"
+        << "======================================*/\n\n";
+
+	// This section is a command-loop repeating the program until the user decides to quit.
+	char command;
+	cout << "Enter command ('h' for list of supported commands): ";
+	while(true) {
+		command = cin.get();
+		cin.ignore(10000, '\n');
+		switch (command) {
+			case 'd': cout << q.dequeue() << " dequeued.\n"; break;	// Dequeues a node from the queue.
+			case 'u': cout << s.pop() << " unstacked.\n"; break;	// Unstack a node from the stack.
+			case 'e': enqueueToQueue(); break; 	// Enqueue a new node to the queue.
+			case 'p': pushToStack(); break; 	// Push a new node to the stack.
+			case 'r': removeFromList(); break; 	// Remove a node from a list.
+			case 'l': displayLists(); break; 	// Display lists' contents.
+			case 'h': printHelpText(); break; 	// Display the help text.
+			case 'q': system("pause"); return 0;// Quit the program.
+		}
+		cout << "Command: ";
+	}
+}
+
+// Enqueue a new node to the queue.
+void enqueueToQueue() {
+	// This section takes the user's input on what the int should be and queues it.
+	int newNum;
+    cout << "Enter value of new node to be enqueued: ";
+    while (!(cin >> newNum)) { // While the input is invalid:
+        cin.clear(); // Clear errors.
+        cin.ignore(10000, '\n'); // Ignore all remaining characters.
+        cout << "Invalid input. Enter again: "; // Let the user know and ask again.
+    }
+	cin.ignore(10000, '\n');
+	q.enqueue(newNum);
+}
+
+// Push a new node to the stack.
+void pushToStack() {
+	// This section takes the user's input on what the int should be and stacks it.
+	int newNum;
+    cout << "Enter value of new node to be pushed: ";
+    while (!(cin >> newNum)) { // While the input is invalid:
+        cin.clear(); // Clear errors.
+        cin.ignore(10000, '\n'); // Ignore all remaining characters.
+        cout << "Invalid input. Enter again: "; // Let the user know and ask again.
+    }
+	cin.ignore(10000, '\n');
+	s.push(newNum);
+}	
+
+// Remove a node from a list.
+void removeFromList() {
+    // This section takes the user's input on whether to remove from the stack or queue.
+	string stackOrQueue;
+    cout << "Remove a node from stack or queue? (s/q): ";
+    getline(cin, stackOrQueue);
+	while (stackOrQueue != "s" && stackOrQueue != "q") {
+		cout << "Invalid input, enter 's' for stack or 'q' for queue: ";
+		getline(cin, stackOrQueue);
+	}
+	
+	// This section takes the user's input on what value should be removed.
+	int delVal;
+    cout << "Enter value of node to be removed: ";
+    while (!(cin >> delVal)) { // While the input is invalid:
+        cin.clear(); // Clear errors.
+        cin.ignore(10000, '\n'); // Ignore all remaining characters.
+        cout << "Invalid input. Enter again: "; // Let the user know and ask again.
+    }
+	cin.ignore(10000, '\n');
+
+	// This section removes the node from the specified list.
+	switch (stackOrQueue[0]) {
+		case 's': s.removeValue(delVal); break;
+		case 'q': q.removeValue(delVal); break;
+	}
+}
+
+// Display lists' contents.
+void displayLists() {
+	cout << "Queue: ";
+	q.display();
+	cout << "Stack: ";
+	s.display();
+}	
+
+// This function displays the help text with the supported commands.
+void printHelpText() {
+	cout<< "Supported commands: \n"
+		<< "     e    Enqueue a new node to the queue.\n" 
+		<< "     d    Dequeue a node from the queue.\n" 
+		<< "     p    Push a new node to the stack.\n" 
+		<< "     u    Unstack a node from the stack.\n" 
+		<< "     r    Remove a node from a list.\n" 
+		<< "     l    Display lists' contents.\n" 
+		<< "     h    Display the help text.\n"
+		<< "     q    Quit the program.\n";
+}
