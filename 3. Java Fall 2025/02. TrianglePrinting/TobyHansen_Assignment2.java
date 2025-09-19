d5fb6a4004fd52a2c929257507798cf8e8836aa3
tobykestrel <tobykestrel@gmail.com>
2025-09-18 21:07:19 -0400

    Completed Java Assignment 2

---
+++ 3. Java Fall 2025/02. TrianglePrinting/TobyHansen_Assignment2.java
@@ -0,0 +1,85 @@
+/*=======================================
+| Toby Hansen, September 18 2025.       |
+| Assignment 2, CSC-285-201.            |
+| This program prints triangles with    |
+| loops, and does simple variable math. |
+=======================================*/
+
+package edu.bhcc.entrypoint;
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class TrianglePrinting {
+	
+	static Scanner in = new Scanner(System.in); 		// Global Scanner.
+	
+	public static void main(String[] args) {
+		
+		// This code banner is printed before the code is run.
+		System.out.print("/*=======================================\n"
+						+"| Toby Hansen, September 18 2025.       |\n"
+						+"| Assignment 2, CSC-285-201.            |\n"
+						+"| This program prints triangles with    |\n"
+						+"| loops, and does simple variable math. |\n"
+						+"=======================================*/\n\n");
+	    
+		// This section is a command-loop repeating the program until the user decides to quit.
+		System.out.print("Enter command ('h' for list of supported commands): ");
+		while(true) {
+			switch (in.next().charAt(0)) {
+				case 'm': doSimpleMath(); break;			// Do math with a given variable.
+				case 'e': printEquilateralTriangle(); break;// Print an equilateral triangle.
+				case 'r': printRightTriangle(); break; 		// Print a right triangle.
+				case 'h': printHelpText(); break; 			// Print the help text.
+				case 'q': System.exit(0);					// Quit the program.
+			}
+			System.out.print("Command: ");
+		}
+	}
+	
+	// This function takes input from the user for formula variables and outputs the solution.
+	static public void doSimpleMath() {
+		System.out.println("The Formula is ((x*15)-3+22)/4");
+		System.out.print("Enter x: ");
+		int x = in.nextInt();
+		int newX = ((x*15)-3+22);
+		int remX = newX % 4;
+		newX = newX/4;
+		System.out.println("New value of x after formula: " + newX);
+		System.out.println("Remainder of x after division: " + remX);
+		if (x > 5) { System.out.println("I am the King (newX > 5)");
+		} else { System.out.println("I Lost (newX <= 5)"); }
+		if (remX > 2) {
+			newX += 10;
+			System.out.println("Value after adding 10 (remX > 2): " + newX); // String.valueOf(newX)
+		}
+	}
+	
+	// This function takes fahrenheit input from the user and outputs the temperature in celsius.
+	static public void printRightTriangle() {
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < (i*2)+1; j++) { System.out.print('*'); }
+			System.out.println("");
+		}
+	}
+
+	// This function takes celsius input from the user and outputs the temperature in fahrenheit.
+	static public void printEquilateralTriangle() {
+		for (int asterisks = 1, spaces = 3; asterisks <= 7; asterisks+=2, spaces--) {
+			for (int i = spaces; i > 0; i--) { System.out.print(' '); }
+			for (int i = asterisks; i > 0; i--) { System.out.print('*'); }
+			System.out.println("");
+		}
+	}
+	
+	// This function prints the help text with the supported commands.
+	static public void printHelpText() {
+		System.out.print("Supported commands:\n"
+				+ "     m    Do math with a given variable.\n"
+				+ "     e    Print an equilateral triangle.\n"
+				+ "     r    Print a right triangle.\n"
+				+ "     h    Print this help text.\n"
+				+ "     q    Quit the program.\n");
+	}
+}
